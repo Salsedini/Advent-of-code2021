115afd02902e60916eb7c303e2a9a3b7cb8bda9f
Salsedini <d.s.022222@gmail.com>
2021-12-01 20:08:52 +0100

    Pa pareser pro

--- Day1/2Star.py
+++ Day1/2Star.py
@@ -8,7 +8,7 @@ with open('fichero', 'r') as f:
 for index,current in enumerate(lista):
     if(index>2):
         sumA=lista[index-3]+lista[index-2]+lista[index-1]
-        sumB=lista[index-2]+lista[index-1]+lista[index]
+        sumB=lista[index-2]+lista[index-1]+current
         if(sumA<sumB):
             result+=1
 print(result)
